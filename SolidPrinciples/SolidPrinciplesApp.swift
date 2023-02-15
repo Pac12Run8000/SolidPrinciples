63d6d11efbea7c132c575fb20d4f8765d6be0300
Pac12Run8000 <gangstarr78992@>
2023-02-15 13:55:12 -0800

    Initial Commit

---
+++ SolidPrinciples/SolidPrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidPrinciplesApp.swift
+//  SolidPrinciples
+//
+//  Created by Michelle Grover on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SolidPrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
